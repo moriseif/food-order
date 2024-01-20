d192d1882465e64436b79251745ac8fec10ff01e
Morteza Seifollah Pour <moriseif@gmail.com>
2024-01-20 22:49:15 +0330

    test

--- routes/admin.routes.ts
+++ routes/admin.routes.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response, NextFunction } from "express";
+import { CreateVendor } from "../controllers";
 
 const router = express.Router();
 
+router.post("/vendor", CreateVendor);
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.json({ message: "hello from admin panel" });
 });
